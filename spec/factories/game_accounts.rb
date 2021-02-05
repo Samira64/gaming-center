1c2d62b01a3182a354770b8d439c8c9e5fec9671
samira64 <s.soltani336@gmail.com>
2021-02-05 12:17:55 +0330

    Add some attributes to game_account factory

--- spec/factories/game_accounts.rb
+++ spec/factories/game_accounts.rb
@@ -2,13 +2,15 @@ FactoryBot.define do
 	factory :game_account do
 		title {Faker::Game.title}
 		username {Faker::Internet.username}
-		email_id {Faker::Internet.email}
 		password {Faker::Internet.password}
+		email_id {Faker::Internet.email}
+		email_password {Faker::Internet.password}
 		platform {Faker::Game.platform}
-		remarks {"SomeRemarks"}
+		remarks {Faker::TvShows::FamilyGuy.quote}
 		cap1_status {'sold'}
 		cap2_status {'available'}
 		cap3_status {'unavailable'}
 	end
-
 end
+
+# created_game_accounts = Factorybot.create_list(:game_account, 25)
