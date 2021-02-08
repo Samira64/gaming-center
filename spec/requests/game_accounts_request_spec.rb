80c2f51cf49db7ec181d95297afb4eb24ce31be5
mosoltan <sahandfx@gmail.com>
2021-02-08 06:21:09 -0800

    Minor Refactor for request specs

--- spec/requests/game_accounts_request_spec.rb
+++ spec/requests/game_accounts_request_spec.rb
@@ -8,47 +8,47 @@ RSpec.describe "GameAccounts", type: :request do
 	end
 
 	context "Get #index" do
-		it 'shows the list of Game Accounts' do
+		it 'Shows the list of Game Accounts' do
 			get admin_game_accounts_path
 			expect(response.body).to include("All Game Accounts");
 		end
   end
 
     context "Get #new" do
-		it "renders a form creating a new GameAccount" do
+		it "Renders a form for creating a new GameAccount" do
 			get new_admin_game_account_path
 			expect(response.body).to include("Create New Game Account")
 		end	
 	end
 
 	context "Get #edit" do
-		let!(:game_account) { FactoryBot.create(:game_account)}
+		let!(:game_account) { FactoryBot.create(:game_account, { title: 'Tomb Raider' }) }
 
-		it "returns game_accounts#edit template" do
+		it "Displays a page for editing a GameAccount" do
 			get edit_admin_game_account_path game_account
 			expect(response.body).to include("Edit Game Account")
+			expect(response.body).to include(game_account.title)
 		end
 	end
 
 	context "PUT 'update/:id" do
-		let!(:game_account) { create(:game_account)}
+		let!(:game_account) { FactoryBot.create(:game_account) }
+		let!(:new_game_params) { FactoryBot.attributes_for(:game_account) }
 
-		before(:each) do
-			put admin_game_account_path(game_account), params: {game_account:{ id: game_account.id, title: "my new title" }}
+	    it "Updates GameAccount details" do
+			put admin_game_account_path(game_account), params: { game_account: new_game_params }
 			game_account.reload
+			expect(response).to redirect_to(admin_game_accounts_path)
+			expect(game_account.title).to eql(new_game_params[:title])
 		end
-		
-	    it { expect(response).to redirect_to(admin_game_accounts_path) }
-	    it { expect(game_account.title).to eql("my new title") }
 		
 	end
 
 	context "Delete #destroy" do
-		let!(:game_account) { create(:game_account)}
+		let!(:game_account) { FactoryBot.create(:game_account)}
 
-		it "Allows a game_account to be deleted" do
-			expect { 
-      		 delete "/admin/game_accounts/#{game_account.id}", params: { id: game_account.id } }.to change(GameAccount, :count).by(-1)		
+		it "Deletes a GameAccount" do
+			expect { delete "/admin/game_accounts/#{game_account.id}" }.to change(GameAccount, :count).by(-1)		
 		end
 	end
 
