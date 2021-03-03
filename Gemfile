03b637436fead65a1f10d473ec5fee599da380e8
samira64 <s.soltani336@gmail.com>
2021-03-03 13:43:01 +0330

    Introduce react and render a test component inside edit form partial

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'faker'
 gem 'hirb'
 gem "selectize-rails"
 gem 'deface'
+gem 'webpacker'
+gem 'react-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {this.props.greeting}
+        <hr/>
+      </React.Fragment>
+    );
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+};
+export default HelloWorld
